a053e295db00a5811a416ba6b386a3051b58ce87
SkidMaster999 <skidmaster999@gmail.com>
2025-07-13 18:00:21 +0800

    Update NeckerRadio.lua

--- Game/NeckerRadio.lua
+++ Game/NeckerRadio.lua
@@ -1,1 +1,13 @@
---A script that plays a list Neck Hurts songs
+--Planning on turning it into "Music Hub"
+local F="NeckHurts1.mp3" 
+local A=getsynasset or getcustomasset
+if not(writefile and isfile and A)then warn("unsupported")return end
+writefile(F,game:HttpGet("https://github.com/SkidMaster999/RobloxObjects/blob/main/audiomass-output%20(10).mp3?raw=true"))
+local S=Instance.new("Sound")
+S.SoundId=A(F)
+S.Volume= 1 
+S.Pitch = 0.5
+S.PlaybackSpeed = 0.5
+S.Parent=game:GetService("SoundService")
+S:Play()
+S.Looped = true
