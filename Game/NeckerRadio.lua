0c0c83ced9a469c2ee5cec29f0447d628167107f
SkidMaster999 <skidmaster999@gmail.com>
2025-07-16 21:26:48 +0800

    Update NeckerRadio.lua

--- Game/NeckerRadio.lua
+++ Game/NeckerRadio.lua
@@ -2,12 +2,10 @@
 local F="NeckHurts1.mp3" 
 local A=getsynasset or getcustomasset
 if not(writefile and isfile and A)then warn("unsupported")return end
-writefile(F,game:HttpGet("https://github.com/SkidMaster999/RobloxObjects/blob/main/audiomass-output%20(10).mp3?raw=true"))
+writefile(F,game:HttpGet("https://github.com/SkidMaster999/RobloxObjects/blob/main/7d012a3c-daf5-498e-a668-65353a0d5ed3.mp3?raw=true"))
 local S=Instance.new("Sound")
 S.SoundId=A(F)
 S.Volume= 1 
-S.Pitch = 0.5
-S.PlaybackSpeed = 0.5
 S.Parent=game:GetService("SoundService")
 S:Play()
 S.Looped = true
