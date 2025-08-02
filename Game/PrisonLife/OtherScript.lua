68747d76ec2c160424f184c023ce65f46dce0d8e
SkidMaster999 <skidmaster999@gmail.com>
2025-08-02 13:29:59 +0800

    Update OtherScript.lua

--- Game/PrisonLife/OtherScript.lua
+++ Game/PrisonLife/OtherScript.lua
@@ -1,8 +1,7 @@
 local addother = ...
---Stil in testing phase
---addother("Prison Life DrawTool",function()
-	--loadstring(game:HttpGet("https://raw.githubusercontent.com/SkidMaster999/RobloxScript/refs/heads/main/Game/PrisonLife/DrawTool.lua"))()
---end)
+addother("Prison Life DrawTool",function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/SkidMaster999/RobloxScript/refs/heads/main/Game/PrisonLife/DrawTool.lua"))()
+end)
 addother("Infinite Yield",function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end)
