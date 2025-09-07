20efe4f97d5089d3610f57ffcd13397ab9a22257
SkidMaster999 <skidmaster999@gmail.com>
2025-09-07 19:36:01 +0800

    Update NeckerRadio.lua

--- Game/NeckerRadio.lua
+++ Game/NeckerRadio.lua
@@ -1,11 +1,18 @@
---Planning on turning it into "Music Hub"
-local F="NeckHurts1.mp3" 
-local A=getsynasset or getcustomasset
-if not(writefile and isfile and A)then warn("unsupported")return end
-writefile(F,game:HttpGet("https://github.com/SkidMaster999/RobloxObjects/blob/main/7d012a3c-daf5-498e-a668-65353a0d5ed3.mp3?raw=true"))
-local S=Instance.new("Sound")
-S.SoundId=A(F)
-S.Volume= 1 
-S.Parent=game:GetService("SoundService")
-S:Play()
-S.Looped = true
+if not(writefile and isfile and A)then warn("unsupported") return _G.TopBarNotif("Unsupported executor!") end
+for i = 0,16 do
+  local F = "neckhurtsong"..tostring(i)..".mp3"
+  if not isfile(F) then
+    task.spawn(function()
+        _G.TopBarNotif("Loading "..F.." please wait...")
+    end)
+    writefile(F,game:HttpGet("https://github.com/SkidMaster999/RobloxObjects/blob/main/"..F.."?raw=true"))
+  end
+  local A=getsynasset or getcustomasset
+  local S=Instance.new("Sound")
+  S.SoundId=A(F)
+  S.Volume= 1 
+  S.Parent=game:GetService("SoundService")
+  S:Play()
+  S.Ended:Wait()
+  S:Destroy()
+end
